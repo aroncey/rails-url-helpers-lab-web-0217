748ba7cb6f34a92ceb98ace6bdc4e448cdff3c41
“aroncey” <artuhrroncey@gmail.comssh-keygen -t rsa -C arthurroncey@gmail.com>
2017-02-28 17:33:22 -0500

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,12 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def change_activation_status
+    self.active == false ? self.active = true : self.active = false
+  end
+
+  def active_status
+    active == true ? "active" : "inactive"
+  end
 end
